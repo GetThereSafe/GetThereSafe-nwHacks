a4d9745512258afdb407cec7f6128f882015e579
Dominique Charlebois <spotdom@hotmail.com>
2016-02-27 17:18:34 -0800

    Website Portion

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,3 @@
 $("#bttn").click(function() {
     alert("hey");
 });
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
-});
