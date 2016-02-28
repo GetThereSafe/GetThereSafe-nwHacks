0d580acf41f6e1fe40702089635ab51632a0b281
Christophe Pouliot <cristophepoug@gmail.com>
2016-02-27 18:01:06 -0800

    added map to website

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,12 @@
 $("#bttn").click(function() {
     alert("hey");
 });
+
+function initMap() {
+    console.log('bla');
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+    });
+}
