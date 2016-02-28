ed911fee22c7aa93d12dd99deb78d92a47972d05
AJ Po-Deziel <aj.podeziel@gmail.com>
2016-02-27 17:29:38 -0800

    Updated with CSV DictReader to read CityLight

--- OrchestrateClient.py
+++ OrchestrateClient.py
@@ -1,3 +1,11 @@
-from porc import Client
+import porc
+import csv
 
-response = client.ping()
+client = porc.Client('767da773-3cc8-4816-9dac-d811fb6767d5')
+
+def read_csv_file(file_name):
+    with open(file_name) as csvfile:
+        csv_data_reader = csv.DictReader(csvfile)
+
+response = client.put(read_csv_file('CityLight.csv'))
+response.raise_for_status()
