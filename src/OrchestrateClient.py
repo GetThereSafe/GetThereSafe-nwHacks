e44a2efd425582d7267c526058728e97891d919a
Dominique Charlebois <spotdom@hotmail.com>
2016-02-27 16:58:30 -0800

    Hamburger Menu Icon

--- src/OrchestrateClient.py
+++
@@ -1,4 +0,0 @@
-import porc
-
-client = porc.Client('767da773-3cc8-4816-9dac-d811fb6767d5')
-response = client.ping()
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,8 @@
 $("#bttn").click(function() {
     alert("hey");
 });
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
